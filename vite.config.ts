ceeec6460b857d54ef2b84ef244b97bdadbe7b88
RadekDulisz <radek@dulisz.com>
2024-09-14 18:44:37 +0200

    project setup

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import Path from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()]
+    }
+  },
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@/components': Path.resolve(__dirname, '@/components'),
+      '@/lib': Path.resolve(__dirname, '@/lib'),
+    }
+  }
+})
